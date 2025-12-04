d526ea2922732780afa55e06c9943440875e4574
HildebrandBD <134274042+HildebrandBD@users.noreply.github.com>
2025-12-04 14:11:05 +0100

    Add AAP provider to providers.tf
    Add AAP provider configuration to Terraform.

--- providers.tf
+++ providers.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+
+    aap = {
+      source = "ansible/aap"
+    }
   }
 }
 
@@ -18,5 +22,3 @@ provider "aws" {
     }
   }
 }
-
-
