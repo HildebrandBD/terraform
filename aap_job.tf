2626c67f5dac848f7892f1390dee8bdb62283738
HildebrandBD <134274042+HildebrandBD@users.noreply.github.com>
2025-12-04 14:59:01 +0100

    Add AAP job resources for webserver and website

--- aap_job.tf
+++ aap_job.tf
@@ -1,1 +1,29 @@
+data "aap_job_template" "DeployWebserver" {
+  name              = "DeployWebserver"
+  organization_name = var.organization_name
+}
 
+data "aap_job_template" "DeployWebsite" {
+  name              = "DeployWebsite"
+  organization_name = var.organization_name
+}
+
+resource "aap_job" "DeployWebserverJob" {
+  inventory_id    = aap_inventory.TechXchangeNL-IHD-TF.id
+  job_template_id = aap_job_template.DeployWebserver.id
+
+  depends_on = [
+    aap_host.TechXchangeNL-IHD-TF-HOST,
+    aap_group.TechXchangeNL-IHD-TF-GRP
+  ]
+}
+
+resource "aap_job" "DeployWebsite" {
+  inventory_id    = aap_inventory.TechXchangeNL-IHD-TF.id
+  job_template_id = aap_job_template.DeployWebsite.id
+
+  depends_on = [
+    aap_host.TechXchangeNL-IHD-TF-HOST,
+    aap_group.TechXchangeNL-IHD-TF-GRP
+  ]
+}
