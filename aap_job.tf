480432e43f1d74a4254d5dd4ea7cb1745ccb6067
HildebrandBD <134274042+HildebrandBD@users.noreply.github.com>
2025-12-04 15:07:15 +0100

    Refactor AAP job resources for deployment
    Refactor job resources for web server and website deployment with new naming conventions and added triggers.

--- aap_job.tf
+++ aap_job.tf
@@ -1,29 +1,28 @@
-data "aap_job_template" "DeployWebserver" {
-  name              = "DeployWebserver"
-  organization_name = var.organization_name
-}
-
-data "aap_job_template" "DeployWebsite" {
-  name              = "DeployWebsite"
-  organization_name = var.organization_name
-}
-
-resource "aap_job" "DeployWebserverJob" {
-  inventory_id    = aap_inventory.TechXchangeNL-IHD-TF.id
-  job_template_id = aap_job_template.DeployWebserver.id
-
+resource "aap_job" "deploy_web_server" {
+  inventory_id = aap_inventory.inventory.id
+  job_template_id = data.aap_job_template.deploy_web_server.id
+  wait_for_completion                 = true
+  wait_for_completion_timeout_seconds = 180
+  triggers = {
+    "aap_job_run_timestamp":timestamp()
+  }
   depends_on = [
-    aap_host.TechXchangeNL-IHD-TF-HOST,
-    aap_group.TechXchangeNL-IHD-TF-GRP
+    aap_host.my_host,
+    aap_group.my_group
   ]
 }
 
-resource "aap_job" "DeployWebsite" {
-  inventory_id    = aap_inventory.TechXchangeNL-IHD-TF.id
-  job_template_id = aap_job_template.DeployWebsite.id
+resource "aap_job" "deploy_web_site" {
+  inventory_id = aap_inventory.inventory.id
+  job_template_id = data.aap_job_template.deploy_web_site.id
+  wait_for_completion                 = true
+  wait_for_completion_timeout_seconds = 180
+
+  triggers = {
+    "aap_job_run_timestamp":timestamp()
+  }
 
   depends_on = [
-    aap_host.TechXchangeNL-IHD-TF-HOST,
-    aap_group.TechXchangeNL-IHD-TF-GRP
+    aap_job.webserver
   ]
 }
