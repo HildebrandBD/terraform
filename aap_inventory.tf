56f78deb19e04799c00bf0916a8f93651305e1e7
HildebrandBD <134274042+HildebrandBD@users.noreply.github.com>
2025-12-04 14:25:11 +0100

    Add AAP inventory, group, and host resources

--- aap_inventory.tf
+++ aap_inventory.tf
@@ -1,1 +1,16 @@
+resource "aap_inventory" "TechXchangeNL-IHD-TF" {
+  name         = "TF-INV"
+  description  = "A new inventory for testing"
+  organization = "TechXchangeNL"
+}
 
+resource "aap_group" "TechXchangeNL-IHD-TF-GRP" {
+  inventory_id = aap_inventory.TechXchangeNL-IHD-TF.id
+  name         = "TF-GRP"
+}
+
+resource "aap_host" "TechXchangeNL-IHD-TF-HOST" {
+  inventory_id = aap_inventory.TechXchangeNL-IHD-TF.id
+  name         = "TF_HOST"
+  groups = [aap_group.TechXchangeNL-IHD-TF-GRP.id]
+}
